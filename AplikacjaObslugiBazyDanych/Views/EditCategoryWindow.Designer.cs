e4f63fd07f1a8715a845969f2d19f02b67e8ab29
Alan <alanbiegun13@gmail.com>
2018-02-19 00:29:28 +0100

    Dodanie podstawowej funkcjonalności edtcji kategorii

--- AplikacjaObslugiBazyDanych/Views/EditCategoryWindow.Designer.cs
+++ AplikacjaObslugiBazyDanych/Views/EditCategoryWindow.Designer.cs
@@ -28,243 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.label7 = new System.Windows.Forms.Label();
-            this.button5 = new System.Windows.Forms.Button();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
-            this.label6 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button4 = new System.Windows.Forms.Button();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
+            this.DataTable = new System.Windows.Forms.DataGridView();
+            this.AddCategory = new System.Windows.Forms.Button();
+            this.RemoveCategory = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.CategoryId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CategoryNam = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ParentCategory = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.DataTable)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // DataTable
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(172, 32);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(83, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Nazwa kategorii";
+            this.DataTable.AllowUserToAddRows = false;
+            this.DataTable.AllowUserToDeleteRows = false;
+            this.DataTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.CategoryId,
+            this.CategoryNam,
+            this.ParentCategory});
+            this.DataTable.Location = new System.Drawing.Point(12, 12);
+            this.DataTable.Name = "DataTable";
+            this.DataTable.RowHeadersVisible = false;
+            this.DataTable.Size = new System.Drawing.Size(637, 537);
+            this.DataTable.TabIndex = 0;
             // 
-            // comboBox1
+            // AddCategory
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(19, 51);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(147, 21);
-            this.comboBox1.TabIndex = 1;
+            this.AddCategory.Location = new System.Drawing.Point(655, 12);
+            this.AddCategory.Name = "AddCategory";
+            this.AddCategory.Size = new System.Drawing.Size(117, 50);
+            this.AddCategory.TabIndex = 1;
+            this.AddCategory.Text = "Dodaj kategorie";
+            this.AddCategory.UseVisualStyleBackColor = true;
+            this.AddCategory.Click += new System.EventHandler(this.AddCategory_Click);
             // 
-            // label2
+            // RemoveCategory
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(172, 59);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(89, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Kategoria rodzica";
+            this.RemoveCategory.Location = new System.Drawing.Point(655, 68);
+            this.RemoveCategory.Name = "RemoveCategory";
+            this.RemoveCategory.Size = new System.Drawing.Size(117, 50);
+            this.RemoveCategory.TabIndex = 2;
+            this.RemoveCategory.Text = "Usuń kategorie";
+            this.RemoveCategory.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // Save
             // 
-            this.textBox1.Location = new System.Drawing.Point(19, 25);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(147, 20);
-            this.textBox1.TabIndex = 3;
+            this.Save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Save.Location = new System.Drawing.Point(655, 396);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(117, 50);
+            this.Save.TabIndex = 3;
+            this.Save.Text = "Zapisz";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
             // 
-            // button1
+            // Exit
             // 
-            this.button1.Location = new System.Drawing.Point(59, 93);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Ok";
-            this.button1.UseVisualStyleBackColor = true;
+            this.Exit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Exit.Location = new System.Drawing.Point(655, 499);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(117, 50);
+            this.Exit.TabIndex = 4;
+            this.Exit.Text = "Zamknij";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
             // 
-            // button2
+            // CategoryId
             // 
-            this.button2.Location = new System.Drawing.Point(156, 93);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "Anuluj";
-            this.button2.UseVisualStyleBackColor = true;
+            this.CategoryId.HeaderText = "Numer kategorii";
+            this.CategoryId.Name = "CategoryId";
+            this.CategoryId.ReadOnly = true;
+            this.CategoryId.Width = 200;
             // 
-            // label3
+            // CategoryNam
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(16, 9);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(104, 13);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Dodawanie kategorii";
+            this.CategoryNam.HeaderText = "Nazwa kategorii";
+            this.CategoryNam.Name = "CategoryNam";
+            this.CategoryNam.Width = 200;
             // 
-            // panel1
+            // ParentCategory
             // 
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.comboBox1);
-            this.panel1.Location = new System.Drawing.Point(12, 243);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(385, 144);
-            this.panel1.TabIndex = 7;
-            // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.label7);
-            this.panel2.Controls.Add(this.button5);
-            this.panel2.Controls.Add(this.comboBox3);
-            this.panel2.Controls.Add(this.label4);
-            this.panel2.Controls.Add(this.label5);
-            this.panel2.Controls.Add(this.button3);
-            this.panel2.Controls.Add(this.label6);
-            this.panel2.Controls.Add(this.textBox2);
-            this.panel2.Controls.Add(this.button4);
-            this.panel2.Controls.Add(this.comboBox2);
-            this.panel2.Location = new System.Drawing.Point(12, 12);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(385, 195);
-            this.panel2.TabIndex = 8;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(172, 44);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(81, 13);
-            this.label7.TabIndex = 9;
-            this.label7.Text = "Wybór kategorii";
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(259, 36);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(97, 23);
-            this.button5.TabIndex = 8;
-            this.button5.Text = "Usuń kategorię";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // comboBox3
-            // 
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(19, 36);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(147, 21);
-            this.comboBox3.TabIndex = 7;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(16, 9);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(82, 13);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Edycja kategorii";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(172, 97);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(89, 13);
-            this.label5.TabIndex = 2;
-            this.label5.Text = "Kategoria rodzica";
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(156, 125);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "Anuluj";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(172, 70);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(83, 13);
-            this.label6.TabIndex = 0;
-            this.label6.Text = "Nazwa kategorii";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(19, 63);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(147, 20);
-            this.textBox2.TabIndex = 3;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(59, 125);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "Ok";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(19, 89);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(147, 21);
-            this.comboBox2.TabIndex = 1;
+            this.ParentCategory.HeaderText = "Kategoria nadrzędna";
+            this.ParentCategory.Name = "ParentCategory";
+            this.ParentCategory.Width = 200;
             // 
             // EditCategoryWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(410, 399);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.RemoveCategory);
+            this.Controls.Add(this.AddCategory);
+            this.Controls.Add(this.DataTable);
             this.Name = "EditCategoryWindow";
             this.Text = "EditCategoryWindow";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DataTable)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.ComboBox comboBox3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.DataGridView DataTable;
+        private System.Windows.Forms.Button AddCategory;
+        private System.Windows.Forms.Button RemoveCategory;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CategoryId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CategoryNam;
+        private System.Windows.Forms.DataGridViewComboBoxColumn ParentCategory;
     }
 }
--- AplikacjaObslugiBazyDanych/Views/EditCategoryWindow.cs
+++ AplikacjaObslugiBazyDanych/Views/EditCategoryWindow.cs
@@ -2,19 +2,105 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity.Migrations;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AplikacjaObslugiBazyDanych.Models;
 
 namespace AplikacjaObslugiBazyDanych.Views
 {
     public partial class EditCategoryWindow : Form
     {
+        DatabaseContext context = new DatabaseContext();
+        private List<Category> categories;
+
         public EditCategoryWindow()
         {
             InitializeComponent();
+
+            categories = context.Categories.ToList();
+
+            var parentCategory = DataTable.Columns[2] as DataGridViewComboBoxColumn;
+
+            parentCategory.Items.Add("brak");
+            foreach (var category in categories)
+            {
+                parentCategory.Items.Add(category.Name);
+            }
+
+            foreach (var category in categories)
+            {
+                DataTable.RowCount++;
+                DataTable.Rows[DataTable.RowCount - 1].Cells[0].Value = category.CategoryId;
+                DataTable.Rows[DataTable.RowCount - 1].Cells[1].Value = category.Name;
+                if (category.Parent != null)
+                {
+                    DataTable.Rows[DataTable.RowCount - 1].Cells[2].Value =
+                        parentCategory.Items[parentCategory.Items.IndexOf(category.Parent.Name)];
+                }
+                else
+                {
+                    DataTable.Rows[DataTable.RowCount - 1].Cells[2].Value =
+                        parentCategory.Items[parentCategory.Items.IndexOf("brak")];
+                }
+            }
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            var count = DataTable.RowCount;
+            for (var i = 0; i < count; i++)
+            {
+                var parent =
+                    categories.FirstOrDefault(a => a.Name.Equals(DataTable.Rows[i].Cells[2].Value));
+
+                if (parent != null || (DataTable.Rows[i].Cells[2].Value != null && DataTable.Rows[i].Cells[2].Value.ToString() == "brak"))
+                {
+                    var category = new Category()
+                    {
+                        CategoryId = (int) DataTable.Rows[i].Cells[0].Value,
+                        Name = DataTable.Rows[i].Cells[1].Value.ToString(),
+                    };
+
+
+                    if (DataTable.Rows[i].Cells[2].Value.ToString() == "brak")
+                    {
+                        category.ParentId = null;
+                    }
+                    else
+                    {
+                        category.ParentId = parent.CategoryId;
+                    }
+
+                    context.Categories.AddOrUpdate(category);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        private void AddCategory_Click(object sender, EventArgs e)
+        {
+            DataTable.RowCount++;
+
+            Category newCategory = new Category()
+            {
+                Name = "Nowa kategoria",
+                ParentId = null,
+            };
+
+            context.Categories.Add(newCategory);
+            context.SaveChanges();
+
+            DataTable.Rows[DataTable.RowCount - 1].Cells[0].Value = newCategory.CategoryId;
+            DataTable.Rows[DataTable.RowCount - 1].Cells[1].Value = "Nowa kategoria";
         }
     }
 }
